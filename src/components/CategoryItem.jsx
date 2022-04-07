af5213e86620db2a04d32b97c773d388c406152c
ruedalibre <andresruedalibre@gmail.com>
2022-04-06 23:45:40 -0500

    Categories section part I

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    flex: 1;
+`
+const Image = styled.img`
+    width: 100%
+`
+const Info = styled.div`
+
+`
+const Title = styled.h1`
+
+`
+const Button = styled.button`
+
+`
+
+
+const CategoryItem = ({item}) => {
+  return (
+    <Container>
+        <Image src={item.img}/>
+        <Info>
+            <Title>{item.title}</Title>
+            <Button>SHOW NOW</Button>
+        </Info>
+
+    </Container>
+  )
+}
+
+
+export default CategoryItem
