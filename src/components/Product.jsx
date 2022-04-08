33d49e5b75d4fbde972c42911cdfa1e6be2141e4
ruedalibre <andresruedalibre@gmail.com>
2022-04-07 19:44:49 -0500

    Products section part II

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,22 +5,49 @@ import styled from 'styled-components'
 const Container = styled.div`
     flex: 1;
     margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd;
+    position: relative;
 `
 
 const Circle = styled.div`
-
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+    position: absolute;
 `
 
 const Image = styled.img`
-
+    height: 75%;
+    z-index: 2;
 `
-
+// Este es el contenedor de los íconos
 const Info = styled.div`
-
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: gray;
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 const Icon = styled.div`
-
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,6 +6,8 @@ import Product from './Product'
 const Container = styled.div`
     padding: 20px;
     display: flex;
+    // Para que las imágenes no se salgan del ancho total del contenedor
+    flex-wrap: wrap;
 `
 
 const Products = () => {
