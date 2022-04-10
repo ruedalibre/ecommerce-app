c0bcc3c2cd7669cef400f53390d7093687940107
ruedalibre <andresruedalibre@gmail.com>
2022-04-10 14:55:20 -0500

    Products Page finished

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,7 @@ import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
+import { Remove, Add } from '@material-ui/icons'
 
 const Container = styled.div`
 
@@ -35,17 +36,65 @@ const Price = styled.span`
     font-size: 40px;
 `
 const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
     display: flex;
+    justify-content: space-between;
 `
 const Filter = styled.div`
+    display: flex;
+    align-items: center
 `
 const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
 `
 const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color}; 
+    margin: 0px 5px;  
+    cursor: pointer;
 `
 const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
 `
 const FilterSizeOption = styled.option`
+`
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer; 
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
 `
 
 const Product = () => {
@@ -78,9 +127,16 @@ const Product = () => {
                                 <FilterSizeOption>XL</FilterSizeOption>
                                 <FilterSizeOption>XXL</FilterSizeOption>
                             </FilterSize>
-
                     </Filter>
                 </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <Remove/>
+                        <Amount>1</Amount>
+                        <Add/>
+                    </AmountContainer>
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
             </InfoContainer>
         </Wrapper>
         <Newsletter/>
