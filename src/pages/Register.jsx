fe5c1caddadbe8097b6ae3895bbc7ab4d25d3201
ruedalibre <andresruedalibre@gmail.com>
2022-04-10 18:56:24 -0500

    Register Page ready

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -23,14 +23,26 @@ const Title = styled.h1`
 `
 
 const Form = styled.form`
-    diplay: flex;
+    display: flex;
     flex-wrap: wrap;
 `
 const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
 `
 const Agreement = styled.span`
+    font-size: 12px; 
+    margin: 20px 0px;
 `
 const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
 `
 
 const Register = () => {
