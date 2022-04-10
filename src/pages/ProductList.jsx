6ebece870e451c6ee102f9e83a9c4dad908da6bb
ruedalibre <andresruedalibre@gmail.com>
2022-04-09 19:26:26 -0500

    Product List Page part II

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -20,6 +20,20 @@ const Filter = styled.div`
     margin: 20px;
 `
 
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`
+const Option = styled.option`
+    padding: 15px;
+`
+
 /* Voy a reusar varios de los componentes del Home ya que se repiten para esta página */
 const ProductList = () => {
   return (
@@ -28,8 +42,39 @@ const ProductList = () => {
         <Navbar/>
         <Title>Dresses</Title>
         <FilterContainer>
-            <Filter>Filter 1</Filter>
-            <Filter>Filter 2</Filter>
+            <Filter>
+                <FilterText>Filter Products:</FilterText>
+                <Select>
+                    <Option disabled selected>
+                        Color
+                    </Option>
+                    <Option>White</Option>
+                    <Option>Red</Option>
+                    <Option>Blue</Option>
+                    <Option>Yellow</Option>
+                    <Option>Black</Option>
+                    <Option>Green</Option>
+                </Select>
+                <Select>
+                    <Option disabled selected>
+                        Size
+                    </Option>
+                    <Option>XS</Option>
+                    <Option>S</Option>
+                    <Option>M</Option>
+                    <Option>L</Option>
+                    <Option>XL</Option>
+                    <Option>XXL</Option>
+                </Select>
+            </Filter>
+            <Filter>
+                <FilterText>Sort Products:</FilterText>
+                <Select>
+                    <Option selected>Newest</Option>
+                    <Option>Price (asc)</Option>
+                    <Option>Price (desc)</Option>
+                </Select>
+            </Filter>
         </FilterContainer>
         <Products/>
         <Newsletter/>
