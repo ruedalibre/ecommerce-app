4e8e863b14e9cc421df968baaf04fd370c6e9657
ruedalibre <andresruedalibre@gmail.com>
2022-04-10 20:35:15 -0500

    Cart Page part II

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,6 +7,47 @@ import Footer from '../components/Footer'
 const Container = styled.div`
 
 `
+const Wrapper = styled.div`
+    padding: 20px
+`
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+
+`
+const Top = styled.div`
+    display: flex;
+    align-items: center; 
+    justify-content: space-between;
+    padding: 20px;
+`
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${props=>props.type === "filled" && "none"};
+    background-color: ${props=>props.type === "filled" ? "black" : "transparent"};
+    color: ${props=>props.type === "filled" && "white"};
+`
+const TopTexts = styled.div`
+`
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px; 
+`
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Info = styled.div`
+    flex: 3;
+
+`
+const Summary = styled.div`
+    flex: 1;
+`
 
 const Cart = () => {
   return (
@@ -15,8 +56,18 @@ const Cart = () => {
         <Navbar/>
         <Wrapper>
             <Title>YOUR BAG</Title>
-            <Top></Top>
-            <Bottom></Bottom>
+            <Top>
+                <TopButton>CONTINUE SHOPPING</TopButton>
+                <TopTexts>
+                    <TopText>Shopping Bag (2)</TopText>
+                    <TopText>Your Wishlist (0)</TopText>
+                </TopTexts>
+                <TopButton type="filled">CHECKOUT NOW</TopButton>
+            </Top>
+            <Bottom>
+                <Info>Info</Info>
+                <Summary>Summary</Summary>
+            </Bottom>
         </Wrapper>
         <Footer/>
 
