089ac4800136b3e15abb2e4292ab6888d198f37f
ruedalibre <andresruedalibre@gmail.com>
2022-04-16 18:08:00 -0500

    routes config part II

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,22 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 
 const App = () => {
+const user = true;
+
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/products/:category" element={<ProductList/>}/>
         <Route path="/product/:id" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}
+          {...user ? <Navigate to="/"/> : <Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   );
